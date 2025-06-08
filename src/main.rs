0bc8ce97006a6176109240ef3d5f8a6231657614
allansomensi <allansomensidev@gmail.com>
2025-06-08 19:48:41 -0300

    feat(piece): :sparkles: draw pieces on the board

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 mod board;
 mod config;
+mod pieces;
 
-use crate::board::draw_board;
 use crate::config::window_conf;
 use macroquad::prelude::*;
 
 #[macroquad::main(window_conf)]
 async fn main() {
+    let config = config::BoardConfig::new();
+
+    let textures = pieces::load_piece_textures().await;
+    let position = shakmaty::Chess::default();
+
     loop {
         clear_background(DARKGRAY);
 
-        draw_board();
+        board::draw_board(&config);
+        pieces::draw_pieces(&position, &textures, &config);
 
         next_frame().await;
     }
